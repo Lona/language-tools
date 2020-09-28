9858a2d351b7d172ab932991fb56f385b090b862
Devin Abbott <devinabbott@gmail.com>
2020-09-28 10:25:35 -0700

    Add templates tests

--- src/languages/__tests__/logic-templates.ts
+++ src/languages/__tests__/logic-templates.ts
@@ -1,22 +1,22 @@
 import { templates } from '../logic'
 
 describe('Logic Templates', () => {
-  const typeAnnotation = { value: 'Number', genericArguments: null }
+  const typeAnnotation = { value: 'Number', genericArguments: [] }
   const numberLiteral = { type: 'integer', value: '1' }
 
   it('generates a string', () => {
-    // const string = templates.string`let x: ${[
-    //   'typeAnnotation',
-    //   typeAnnotation,
-    // ]} = ${['numberLiteral', numberLiteral]}`
-    // expect(string).toEqual(`let x: Number = 1`)
+    const string = templates.string`let x: ${[
+      'typeAnnotation',
+      typeAnnotation,
+    ]} = ${['numberLiteral', numberLiteral]}`
+    expect(string).toEqual(`let x: Number = 1`)
   })
 
   it('generates a node', () => {
-    // const node = templates.node.variableDeclaration`let x: ${[
-    //   'typeAnnotation',
-    //   typeAnnotation,
-    // ]} = ${['numberLiteral', numberLiteral]}`
-    // expect(node).toMatchSnapshot()
+    const node = templates.node.variableDeclaration`let x: ${[
+      'typeAnnotation',
+      typeAnnotation,
+    ]} = ${['numberLiteral', numberLiteral]}`
+    expect(node).toMatchSnapshot()
   })
 })
